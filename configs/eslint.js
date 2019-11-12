dd3e9a076243234ae05a4bc1f79a1ae6cafae7b3
Michael Hayes <michael.hayes@airbnb.com>
2019-11-11 16:58:49 -0800

    update lint rule

--- configs/eslint.js
+++ configs/eslint.js
@@ -3,6 +3,7 @@ module.exports = {
     'class-methods-use-this': 'off',
     'no-restricted-syntax': 'off',
     'import/prefer-default-export': 'off',
+    'react/jsx-no-literals': 'off',
   },
   overrides: [
     {
