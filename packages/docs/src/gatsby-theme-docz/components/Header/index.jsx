ca69ef0113e5de9a162d20570eebf00d2f3f6991
Michael Hayes <michael.hayes@airbnb.com>
2019-11-11 16:51:06 -0800

    Add header

---
+++ packages/docs/src/gatsby-theme-docz/components/Header/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const Header = () => (
+  <div
+    style={{
+      'background-image': 'url(/public/giraffe-background.svg)',
+      height: '100px',
+      'background-position': 'bottom left',
+      'background-repeat': 'no-repeat',
+      'background-size': 'contain',
+      'background-color': '#ffdc61',
+      'padding-left': '75px',
+    }}
+  >
+    <h1>GiraphQL</h1>
+  </div>
+);
