52db73767dd5186e9cd6653f5d8d8dc625d78566
Michael Hayes <michael.hayes@airbnb.com>
2020-03-25 11:21:38 -0700

    Fix context arg for smart subscriptions

--- packages/plugin-smart-subscriptions/src/global-types.ts
+++ packages/plugin-smart-subscriptions/src/global-types.ts
@@ -41,7 +41,7 @@ declare global {
         subscriptions: SubscriptionManager,
         parent: unknown,
         args: {},
-        context: object,
+        context: Types['Context'],
         info: GraphQLResolveInfo,
       ) => void;
     }
