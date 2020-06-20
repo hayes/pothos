3eeb58fa6964e8084985d4ad1720ba115d016ed0
Michael Hayes <michael.hayes@airbnb.com>
2020-06-19 18:21:59 -0700

    Add types for field config objects

--- packages/core/src/types/configs.ts
+++ packages/core/src/types/configs.ts
@@ -11,8 +11,20 @@ import {
   GraphQLEnumTypeConfig,
   GraphQLScalarTypeConfig,
   GraphQLInputObjectTypeConfig,
+  GraphQLFieldConfig,
+  GraphQLInputFieldConfig,
 } from 'graphql';
-import { SchemaTypes, ObjectParam } from '..';
+import {
+  SchemaTypes,
+  ObjectParam,
+  OutputType,
+  InputType,
+  FieldNullability,
+  InputFieldMap,
+  TypeParam,
+  FieldRequiredness,
+  InputTypeParam,
+} from '..';
 
 export interface GiraphQLQueryTypeConfig
   extends Omit<GraphQLObjectTypeConfig<unknown, object>, 'fields' | 'interfaces'> {
@@ -91,3 +103,121 @@ export type GiraphQLKindToGraphQLType<T extends GiraphQLTypeKind> = {
   Scalar: GraphQLScalarType;
   InputObject: GraphQLInputObjectType;
 }[T];
+
+export interface GiraphQLObjectFieldConfig<Types extends SchemaTypes>
+  extends Omit<GraphQLFieldConfig<unknown, object>, 'type'> {
+  kind: 'ObjectField';
+  type: GiraphQLOutputFieldConfig<Types>;
+  options: GiraphQLSchemaTypes.ObjectFieldOptions<
+    Types,
+    unknown,
+    TypeParam<Types>,
+    FieldNullability<[unknown]>,
+    InputFieldMap,
+    unknown
+  >;
+}
+
+export interface GiraphQLQueryFieldConfig<Types extends SchemaTypes>
+  extends Omit<GraphQLFieldConfig<unknown, object>, 'type'> {
+  kind: 'QueryField';
+  type: GiraphQLOutputFieldConfig<Types>;
+  options: GiraphQLSchemaTypes.QueryFieldOptions<
+    Types,
+    TypeParam<Types>,
+    FieldNullability<[unknown]>,
+    InputFieldMap,
+    unknown
+  >;
+}
+
+export interface GiraphQLMutationFieldConfig<Types extends SchemaTypes>
+  extends Omit<GraphQLFieldConfig<unknown, object>, 'type'> {
+  kind: 'MutationField';
+  type: GiraphQLOutputFieldConfig<Types>;
+  options: GiraphQLSchemaTypes.MutationFieldOptions<
+    Types,
+    TypeParam<Types>,
+    FieldNullability<[unknown]>,
+    InputFieldMap,
+    unknown
+  >;
+}
+
+export interface GiraphQLSubscriptionFieldConfig<Types extends SchemaTypes>
+  extends Omit<GraphQLFieldConfig<unknown, object>, 'type'> {
+  kind: 'SubscriptionField';
+  type: GiraphQLOutputFieldConfig<Types>;
+  options: GiraphQLSchemaTypes.SubscriptionFieldOptions<
+    Types,
+    TypeParam<Types>,
+    FieldNullability<[unknown]>,
+    InputFieldMap,
+    unknown,
+    unknown
+  >;
+}
+
+export interface GiraphQLInterfaceFieldConfig<Types extends SchemaTypes>
+  extends Omit<GraphQLFieldConfig<unknown, object>, 'type'> {
+  kind: 'InterfaceField';
+  type: GiraphQLOutputFieldConfig<Types>;
+  options: GiraphQLSchemaTypes.InterfaceFieldOptions<
+    Types,
+    unknown,
+    TypeParam<Types>,
+    FieldNullability<[unknown]>,
+    InputFieldMap,
+    unknown
+  >;
+}
+
+export interface GiraphQLInputFieldConfig<Types extends SchemaTypes>
+  extends Omit<GraphQLInputFieldConfig, 'type'> {
+  kind: 'InputField';
+  type: GiraphQLInputFieldConfig<Types>;
+  options: GiraphQLSchemaTypes.InputFieldOptions<
+    Types,
+    InputTypeParam<Types>,
+    FieldRequiredness<[unknown]>
+  >;
+}
+
+export type GiraphQLFieldConfig<Types extends SchemaTypes> =
+  | GiraphQLObjectFieldConfig<Types>
+  | GiraphQLQueryFieldConfig<Types>
+  | GiraphQLMutationFieldConfig<Types>
+  | GiraphQLSubscriptionFieldConfig<Types>
+  | GiraphQLInterfaceFieldConfig<Types>
+  | GiraphQLInputFieldConfig<Types>;
+
+export type GiraphQLOutputFieldConfig<Types extends SchemaTypes> = Exclude<
+  GiraphQLFieldConfig<Types>,
+  { kind: 'InputField' }
+>;
+
+export type GiraphQLFieldKind = GiraphQLTypeConfig['kind'];
+
+export type GiraphQLOutputFieldType<Types extends SchemaTypes> =
+  | {
+      kind: 'Object' | 'Interface' | 'Union' | 'Enum' | 'Scalar';
+      ref: OutputType<Types>;
+      nullable: boolean;
+    }
+  | {
+      kind: 'List';
+      type: Exclude<GiraphQLFieldKind, { kind: 'List' }>;
+      nullable: boolean;
+    };
+
+export type GiraphQLInputFieldType<Types extends SchemaTypes> =
+  | {
+      kind: 'InputObject' | 'Enum' | 'Scalar';
+      ref: InputType<Types>;
+      required: boolean;
+    }
+  | {
+      kind: 'List';
+      type: Exclude<GiraphQLFieldKind, { kind: 'List' }>;
+      required: boolean;
+    };
