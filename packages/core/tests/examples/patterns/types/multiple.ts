1b0d6f89348a4a076f024564bbcb53a8fbf7a224
Michael Hayes <michael@hayes.io>
2021-06-09 17:49:12 -0700

    docs: add example of field helpers for adding common fields to types

---
+++ packages/core/tests/examples/patterns/types/multiple.ts
@@ -0,0 +1,62 @@
+import { ArgBuilder, ObjectRef } from '../../../../src';
+import InputFieldBuilder from '../../../../src/fieldUtils/input';
+import builder, { TypesWithDefault } from '../builder';
+
+function addCommonFields(refs: ObjectRef<unknown, { id: string }>[]) {
+  for (const ref of refs) {
+    builder.objectFields(ref, (t) => ({
+      id: t.exposeID('id', {}),
+      idLength: t.int({
+        resolve: (parent) => parent.id.length,
+      }),
+    }));
+  }
+}
+
+const WithCommonFields1 = builder.objectRef<{ id: string }>('WithCommonFields1').implement({});
+const WithCommonFields2 = builder.objectRef<{ id: string }>('WithCommonFields2').implement({});
+
+addCommonFields([WithCommonFields1, WithCommonFields2]);
+
+function createCommonArgs(arg: ArgBuilder<TypesWithDefault>) {
+  return {
+    id: arg.id({}),
+    reason: arg({ type: 'String', required: false }),
+  };
+}
+
+function createInputFields(t: InputFieldBuilder<TypesWithDefault, 'InputObject'>) {
+  return {
+    id: t.id({}),
+    reason: t.field({ type: 'String', required: false }),
+  };
+}
+
+builder.mutationType({
+  fields: (t) => ({
+    mutation1: t.boolean({
+      args: {
+        ...createCommonArgs(t.arg),
+      },
+      resolve: (parent, args) => !!args.reason,
+    }),
+    mutation2: t.boolean({
+      args: {
+        ...createCommonArgs(t.arg),
+      },
+      resolve: (parent, args) => !!args.reason,
+    }),
+  }),
+});
+
+builder.inputType('InputWithCommonFields1', {
+  fields: (t) => ({
+    ...createInputFields(t),
+  }),
+});
+
+builder.inputType('InputWithCommonFields2', {
+  fields: (t) => ({
+    ...createInputFields(t),
+  }),
+});
