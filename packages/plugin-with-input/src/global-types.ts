leggenda47/giraphql

feature: add with-input plugin

packages/plugin-with-input/src/global-types.ts
@@ -1,0 +1,137 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {
+  FieldNullability,
+  InputFieldMap,
+  InterfaceParam,
+  ObjectParam,
+  OutputShape,
+  SchemaTypes,
+  TypeParam,
+} from '@giraphql/core';
+import {
+  InterfaceFieldWithInputOptions,
+  MutationFieldWithInputOptions,
+  ObjectFieldWithInputOptions,
+  QueryFieldWithInputOptions,
+  SubscriptionFieldWithInputOptions,
+  WithInputBuilderOptions,
+  WithInputInputOptions,
+} from './types';
+import { GiraphQLWithInputPlugin } from '.';
+
+declare global {
+  export namespace GiraphQLSchemaTypes {
+    export interface Plugins<Types extends SchemaTypes> {
+      withInput: GiraphQLWithInputPlugin<Types>;
+    }
+
+    export interface SchemaBuilderOptions<Types extends SchemaTypes> {
+      withInput: WithInputBuilderOptions<Types>;
+    }
+
+    export interface SchemaBuilder<Types extends SchemaTypes> {
+      queryFieldWithInput: <
+        Fields extends InputFieldMap,
+        Type extends TypeParam<Types>,
+        Nullable extends FieldNullability<Type>,
+        ResolveReturnShape,
+        InputName extends string = 'input'
+      >(
+        name: string,
+        inputOptions: WithInputInputOptions<Types, Fields, InputName>,
+        fieldOptions: QueryFieldWithInputOptions<
+          Types,
+          Fields,
+          Type,
+          Nullable,
+          InputName,
+          ResolveReturnShape
+        >,
+      ) => void;
+
+      mutationFieldWithInput: <
+        Fields extends InputFieldMap,
+        Type extends TypeParam<Types>,
+        Nullable extends FieldNullability<Type>,
+        ResolveReturnShape,
+        InputName extends string = 'input'
+      >(
+        name: string,
+        inputOptions: WithInputInputOptions<Types, Fields, InputName>,
+        fieldOptions: MutationFieldWithInputOptions<
+          Types,
+          Fields,
+          Type,
+          Nullable,
+          InputName,
+          ResolveReturnShape
+        >,
+      ) => void;
+
+      subscriptionFieldWithInput: <
+        Fields extends InputFieldMap,
+        Type extends TypeParam<Types>,
+        Nullable extends FieldNullability<Type>,
+        ResolveShape,
+        ResolveReturnShape,
+        InputName extends string = 'input'
+      >(
+        name: string,
+        inputOptions: WithInputInputOptions<Types, Fields, InputName>,
+        fieldOptions: SubscriptionFieldWithInputOptions<
+          Types,
+          Fields,
+          Type,
+          Nullable,
+          InputName,
+          ResolveShape,
+          ResolveReturnShape
+        >,
+      ) => void;
+
+      objectFieldWithInput: <
+        Fields extends InputFieldMap,
+        ParentType extends ObjectParam<Types>,
+        Type extends TypeParam<Types>,
+        Nullable extends FieldNullability<Type>,
+        ResolveReturnShape,
+        InputName extends string = 'input'
+      >(
+        ref: ParentType,
+        name: string,
+        inputOptions: WithInputInputOptions<Types, Fields, InputName>,
+        fieldOptions: ObjectFieldWithInputOptions<
+          Types,
+          OutputShape<Types, ParentType>,
+          Fields,
+          Type,
+          Nullable,
+          InputName,
+          ResolveReturnShape
+        >,
+      ) => void;
+
+      interfaceFieldWithInput: <
+        Fields extends InputFieldMap,
+        ParentType extends InterfaceParam<Types>,
+        Type extends TypeParam<Types>,
+        Nullable extends FieldNullability<Type>,
+        ResolveReturnShape,
+        InputName extends string = 'input'
+      >(
+        ref: ParentType,
+        name: string,
+        inputOptions: WithInputInputOptions<Types, Fields, InputName>,
+        fieldOptions: InterfaceFieldWithInputOptions<
+          Types,
+          OutputShape<Types, ParentType>,
+          Fields,
+          Type,
+          Nullable,
+          InputName,
+          ResolveReturnShape
+        >,
+      ) => void;
+    }
+  }
+}

packages/plugin-with-input/src/index.ts
@@ -1,0 +1,13 @@
+import './global-types';
+import './schema-builder';
+import SchemaBuilder, { BasePlugin, SchemaTypes } from '@giraphql/core';
+
+export * from './types';
+
+const pluginName = 'withInput' as const;
+
+export default pluginName;
+
+export class GiraphQLWithInputPlugin<Types extends SchemaTypes> extends BasePlugin<Types> {}
+
+SchemaBuilder.registerPlugin(pluginName, GiraphQLWithInputPlugin);

packages/plugin-with-input/src/schema-builder.ts
@@ -1,0 +1,157 @@
+import SchemaBuilder, { SchemaTypes } from '@giraphql/core';
+
+const schemaBuilderProto = SchemaBuilder.prototype as GiraphQLSchemaTypes.SchemaBuilder<SchemaTypes>;
+
+function capitalize(s: string) {
+  return `${s.slice(0, 1).toUpperCase()}${s.slice(1)}`;
+}
+
+schemaBuilderProto.queryFieldWithInput = function queryFieldWithInput(
+  fieldName,
+  {
+    name: inputName = `${capitalize(fieldName)}Input`,
+    argName = 'input',
+    inputFields,
+    ...inputOptions
+  },
+  fieldOptions,
+) {
+  const {
+    withInput: { inputArgOptions },
+  } = this.options;
+
+  const inputRef = this.inputType(inputName, {
+    ...inputOptions,
+    fields: inputFields,
+  });
+
+  this.queryField(fieldName, (t) =>
+    t.field({
+      ...fieldOptions,
+      // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+      args: {
+        [argName]: t.arg({ ...(inputArgOptions as {}), type: inputRef, required: true }),
+      } as never,
+    }),
+  );
+};
+
+schemaBuilderProto.mutationFieldWithInput = function mutationFieldWithInput(
+  fieldName,
+  {
+    name: inputName = `${capitalize(fieldName)}Input`,
+    argName = 'input',
+    inputFields,
+    ...inputOptions
+  },
+  fieldOptions,
+) {
+  const {
+    withInput: { inputArgOptions },
+  } = this.options;
+
+  const inputRef = this.inputType(inputName, {
+    ...inputOptions,
+    fields: inputFields,
+  });
+
+  this.mutationField(fieldName, (t) =>
+    t.field({
+      ...fieldOptions,
+      // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+      args: {
+        [argName]: t.arg({ ...(inputArgOptions as {}), type: inputRef, required: true }),
+      } as never,
+    }),
+  );
+};
+
+schemaBuilderProto.subscriptionFieldWithInput = function subscriptionFieldWithInput(
+  fieldName,
+  {
+    name: inputName = `${capitalize(fieldName)}Input`,
+    argName = 'input',
+    inputFields,
+    ...inputOptions
+  },
+  fieldOptions,
+) {
+  const {
+    withInput: { inputArgOptions },
+  } = this.options;
+
+  const inputRef = this.inputType(inputName, {
+    ...inputOptions,
+    fields: inputFields,
+  });
+
+  this.subscriptionField(fieldName, (t) =>
+    t.field({
+      ...fieldOptions,
+      // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+      args: {
+        [argName]: t.arg({ ...(inputArgOptions as {}), type: inputRef, required: true }),
+      } as never,
+    }),
+  );
+};
+
+schemaBuilderProto.objectFieldWithInput = function objectFieldWithInput(
+  ref,
+  fieldName,
+  {
+    name: inputName = `${capitalize(fieldName)}Input`,
+    argName = 'input',
+    inputFields,
+    ...inputOptions
+  },
+  fieldOptions,
+) {
+  const {
+    withInput: { inputArgOptions },
+  } = this.options;
+
+  const inputRef = this.inputType(inputName, {
+    ...inputOptions,
+    fields: inputFields,
+  });
+
+  this.objectField(ref, fieldName, (t) =>
+    t.field({
+      ...fieldOptions,
+      args: {
+        [argName]: t.arg({ ...(inputArgOptions as {}), type: inputRef, required: true }),
+      },
+    } as never),
+  );
+};
+
+schemaBuilderProto.interfaceFieldWithInput = function interfaceFieldWithInput(
+  ref,
+  fieldName,
+  {
+    name: inputName = `${capitalize(fieldName)}Input`,
+    argName = 'input',
+    inputFields,
+    ...inputOptions
+  },
+  fieldOptions,
+) {
+  const {
+    withInput: { inputArgOptions },
+  } = this.options;
+
+  const inputRef = this.inputType(inputName, {
+    ...inputOptions,
+    fields: inputFields,
+  });
+
+  this.interfaceField(ref, fieldName, (t) =>
+    t.field({
+      ...fieldOptions,
+      args: {
+        [argName]: t.arg({ ...(inputArgOptions as {}), type: inputRef, required: true }),
+      },
+    } as never),
+  );
+};

packages/plugin-with-input/src/types.ts
@@ -1,0 +1,132 @@
+import {
+  FieldNullability,
+  InputFieldMap,
+  InputFieldRef,
+  InputRef,
+  InputShapeFromFields,
+  SchemaTypes,
+  TypeParam,
+} from '@giraphql/core';
+
+export interface WithInputBuilderOptions<Types extends SchemaTypes> {
+  inputArgOptions: Omit<
+    GiraphQLSchemaTypes.ArgFieldOptions<Types, InputRef<{}>, true>,
+    'fields' | 'required' | 'type'
+  >;
+}
+
+export type WithInputInputOptions<
+  Types extends SchemaTypes,
+  Fields extends InputFieldMap,
+  InputName extends string
+> = Omit<GiraphQLSchemaTypes.InputObjectTypeOptions<Types, Fields>, 'fields'> & {
+  name?: string;
+  argName?: InputName;
+  inputFields: (t: GiraphQLSchemaTypes.InputFieldBuilder<Types, 'InputObject'>) => Fields;
+};
+
+export type QueryFieldWithInputOptions<
+  Types extends SchemaTypes,
+  Fields extends InputFieldMap,
+  Type extends TypeParam<Types>,
+  Nullable extends FieldNullability<Type>,
+  InputName extends string,
+  ResolveReturnShape
+> = Omit<
+  GiraphQLSchemaTypes.QueryFieldOptions<
+    Types,
+    Type,
+    Nullable,
+    {
+      [K in InputName]: InputFieldRef<InputShapeFromFields<Fields>>;
+    },
+    ResolveReturnShape
+  >,
+  'args'
+>;
+
+export type MutationFieldWithInputOptions<
+  Types extends SchemaTypes,
+  Fields extends InputFieldMap,
+  Type extends TypeParam<Types>,
+  Nullable extends FieldNullability<Type>,
+  InputName extends string,
+  ResolveReturnShape
+> = Omit<
+  GiraphQLSchemaTypes.MutationFieldOptions<
+    Types,
+    Type,
+    Nullable,
+    {
+      [K in InputName]: InputFieldRef<InputShapeFromFields<Fields>>;
+    },
+    ResolveReturnShape
+  >,
+  'args'
+>;
+
+export type SubscriptionFieldWithInputOptions<
+  Types extends SchemaTypes,
+  Fields extends InputFieldMap,
+  Type extends TypeParam<Types>,
+  Nullable extends FieldNullability<Type>,
+  InputName extends string,
+  ResolveShape,
+  ResolveReturnShape
+> = Omit<
+  GiraphQLSchemaTypes.SubscriptionFieldOptions<
+    Types,
+    Type,
+    Nullable,
+    {
+      [K in InputName]: InputFieldRef<InputShapeFromFields<Fields>>;
+    },
+    ResolveShape,
+    ResolveReturnShape
+  >,
+  'args'
+>;
+
+export type ObjectFieldWithInputOptions<
+  Types extends SchemaTypes,
+  ParentShape,
+  Fields extends InputFieldMap,
+  Type extends TypeParam<Types>,
+  Nullable extends FieldNullability<Type>,
+  InputName extends string,
+  ResolveReturnShape
+> = Omit<
+  GiraphQLSchemaTypes.ObjectFieldOptions<
+    Types,
+    ParentShape,
+    Type,
+    Nullable,
+    {
+      [K in InputName]: InputFieldRef<InputShapeFromFields<Fields>>;
+    },
+    ResolveReturnShape
+  >,
+  'args'
+>;
+
+export type InterfaceFieldWithInputOptions<
+  Types extends SchemaTypes,
+  ParentShape,
+  Fields extends InputFieldMap,
+  Type extends TypeParam<Types>,
+  Nullable extends FieldNullability<Type>,
+  InputName extends string,
+  ResolveReturnShape
+> = Omit<
+  GiraphQLSchemaTypes.InterfaceFieldOptions<
+    Types,
+    ParentShape,
+    Type,
+    Nullable,
+    {
+      [K in InputName]: InputFieldRef<InputShapeFromFields<Fields>>;
+    },
+    ResolveReturnShape
+  >,
+  'args'
+>;

packages/plugin-with-input/tests/example/builder.ts
@@ -1,0 +1,11 @@
+import SchemaBuilder from '@giraphql/core';
+import WithInputPlugin from '../../src';
+
+export default new SchemaBuilder({
+  plugins: [WithInputPlugin],
+  withInput: {
+    inputArgOptions: {
+      description: 'input arg',
+    },
+  },
+});

hayes/pothos

packages/plugin-with-input/src/global-types.ts
@@ -1,0 +1,136 @@
+import {
+  FieldNullability,
+  InputFieldMap,
+  InterfaceParam,
+  ObjectParam,
+  OutputShape,
+  SchemaTypes,
+  TypeParam,
+} from '@pothos/core';
+import {
+  InterfaceFieldWithInputOptions,
+  MutationFieldWithInputOptions,
+  ObjectFieldWithInputOptions,
+  QueryFieldWithInputOptions,
+  SubscriptionFieldWithInputOptions,
+  WithInputBuilderOptions,
+  WithInputInputOptions,
+} from './types';
+import { PothosWithInputPlugin } from '.';
+
+declare global {
+  export namespace PothosSchemaTypes {
+    export interface Plugins<Types extends SchemaTypes> {
+      withInput: PothosWithInputPlugin<Types>;
+    }
+
+    export interface SchemaBuilderOptions<Types extends SchemaTypes> {
+      withInput: WithInputBuilderOptions<Types>;
+    }
+
+    export interface SchemaBuilder<Types extends SchemaTypes> {
+      queryFieldWithInput: <
+        Fields extends InputFieldMap,
+        Type extends TypeParam<Types>,
+        Nullable extends FieldNullability<Type>,
+        ResolveReturnShape,
+        InputName extends string = 'input',
+      >(
+        name: string,
+        inputOptions: WithInputInputOptions<Types, Fields, InputName>,
+        fieldOptions: QueryFieldWithInputOptions<
+          Types,
+          Fields,
+          Type,
+          Nullable,
+          InputName,
+          ResolveReturnShape
+        >,
+      ) => void;
+
+      mutationFieldWithInput: <
+        Fields extends InputFieldMap,
+        Type extends TypeParam<Types>,
+        Nullable extends FieldNullability<Type>,
+        ResolveReturnShape,
+        InputName extends string = 'input',
+      >(
+        name: string,
+        inputOptions: WithInputInputOptions<Types, Fields, InputName>,
+        fieldOptions: MutationFieldWithInputOptions<
+          Types,
+          Fields,
+          Type,
+          Nullable,
+          InputName,
+          ResolveReturnShape
+        >,
+      ) => void;
+
+      subscriptionFieldWithInput: <
+        Fields extends InputFieldMap,
+        Type extends TypeParam<Types>,
+        Nullable extends FieldNullability<Type>,
+        ResolveShape,
+        ResolveReturnShape,
+        InputName extends string = 'input',
+      >(
+        name: string,
+        inputOptions: WithInputInputOptions<Types, Fields, InputName>,
+        fieldOptions: SubscriptionFieldWithInputOptions<
+          Types,
+          Fields,
+          Type,
+          Nullable,
+          InputName,
+          ResolveShape,
+          ResolveReturnShape
+        >,
+      ) => void;
+
+      objectFieldWithInput: <
+        Fields extends InputFieldMap,
+        ParentType extends ObjectParam<Types>,
+        Type extends TypeParam<Types>,
+        Nullable extends FieldNullability<Type>,
+        ResolveReturnShape,
+        InputName extends string = 'input',
+      >(
+        ref: ParentType,
+        name: string,
+        inputOptions: WithInputInputOptions<Types, Fields, InputName>,
+        fieldOptions: ObjectFieldWithInputOptions<
+          Types,
+          OutputShape<Types, ParentType>,
+          Fields,
+          Type,
+          Nullable,
+          InputName,
+          ResolveReturnShape
+        >,
+      ) => void;
+
+      interfaceFieldWithInput: <
+        Fields extends InputFieldMap,
+        ParentType extends InterfaceParam<Types>,
+        Type extends TypeParam<Types>,
+        Nullable extends FieldNullability<Type>,
+        ResolveReturnShape,
+        InputName extends string = 'input',
+      >(
+        ref: ParentType,
+        name: string,
+        inputOptions: WithInputInputOptions<Types, Fields, InputName>,
+        fieldOptions: InterfaceFieldWithInputOptions<
+          Types,
+          OutputShape<Types, ParentType>,
+          Fields,
+          Type,
+          Nullable,
+          InputName,
+          ResolveReturnShape
+        >,
+      ) => void;
+    }
+  }
+}

packages/plugin-with-input/src/index.ts
@@ -1,0 +1,13 @@
+import './global-types';
+import './schema-builder';
+import SchemaBuilder, { BasePlugin, SchemaTypes } from '@pothos/core';
+
+export * from './types';
+
+const pluginName = 'withInput' as const;
+
+export default pluginName;
+
+export class PothosWithInputPlugin<Types extends SchemaTypes> extends BasePlugin<Types> {}
+
+SchemaBuilder.registerPlugin(pluginName, PothosWithInputPlugin);

packages/plugin-with-input/src/schema-builder.ts
@@ -1,0 +1,154 @@
+import SchemaBuilder, { SchemaTypes } from '@pothos/core';
+
+const schemaBuilderProto = SchemaBuilder.prototype as PothosSchemaTypes.SchemaBuilder<SchemaTypes>;
+
+function capitalize(s: string) {
+  return `${s.slice(0, 1).toUpperCase()}${s.slice(1)}`;
+}
+
+schemaBuilderProto.queryFieldWithInput = function queryFieldWithInput(
+  fieldName,
+  {
+    name: inputName = `${capitalize(fieldName)}Input`,
+    argName = 'input',
+    inputFields,
+    ...inputOptions
+  },
+  fieldOptions,
+) {
+  const {
+    withInput: { inputArgOptions },
+  } = this.options;
+
+  const inputRef = this.inputType(inputName, {
+    ...inputOptions,
+    fields: inputFields,
+  });
+
+  this.queryField(fieldName, (t) =>
+    t.field({
+      ...fieldOptions,
+      args: {
+        [argName]: t.arg({ ...(inputArgOptions as {}), type: inputRef, required: true }),
+      },
+    } as never),
+  );
+};
+
+schemaBuilderProto.mutationFieldWithInput = function mutationFieldWithInput(
+  fieldName,
+  {
+    name: inputName = `${capitalize(fieldName)}Input`,
+    argName = 'input',
+    inputFields,
+    ...inputOptions
+  },
+  fieldOptions,
+) {
+  const {
+    withInput: { inputArgOptions },
+  } = this.options;
+
+  const inputRef = this.inputType(inputName, {
+    ...inputOptions,
+    fields: inputFields,
+  });
+
+  this.mutationField(fieldName, (t) =>
+    t.field({
+      ...fieldOptions,
+      args: {
+        [argName]: t.arg({ ...(inputArgOptions as {}), type: inputRef, required: true }),
+      },
+    } as never),
+  );
+};
+
+schemaBuilderProto.subscriptionFieldWithInput = function subscriptionFieldWithInput(
+  fieldName,
+  {
+    name: inputName = `${capitalize(fieldName)}Input`,
+    argName = 'input',
+    inputFields,
+    ...inputOptions
+  },
+  fieldOptions,
+) {
+  const {
+    withInput: { inputArgOptions },
+  } = this.options;
+
+  const inputRef = this.inputType(inputName, {
+    ...inputOptions,
+    fields: inputFields,
+  });
+
+  this.subscriptionField(fieldName, (t) =>
+    t.field({
+      ...fieldOptions,
+      args: {
+        [argName]: t.arg({ ...(inputArgOptions as {}), type: inputRef, required: true }),
+      },
+    } as never),
+  );
+};
+
+schemaBuilderProto.objectFieldWithInput = function objectFieldWithInput(
+  ref,
+  fieldName,
+  {
+    name: inputName = `${capitalize(fieldName)}Input`,
+    argName = 'input',
+    inputFields,
+    ...inputOptions
+  },
+  fieldOptions,
+) {
+  const {
+    withInput: { inputArgOptions },
+  } = this.options;
+
+  const inputRef = this.inputType(inputName, {
+    ...inputOptions,
+    fields: inputFields,
+  });
+
+  this.objectField(ref, fieldName, (t) =>
+    t.field({
+      ...fieldOptions,
+      args: {
+        [argName]: t.arg({ ...(inputArgOptions as {}), type: inputRef, required: true }),
+      },
+    } as never),
+  );
+};
+
+schemaBuilderProto.interfaceFieldWithInput = function interfaceFieldWithInput(
+  ref,
+  fieldName,
+  {
+    name: inputName = `${capitalize(fieldName)}Input`,
+    argName = 'input',
+    inputFields,
+    ...inputOptions
+  },
+  fieldOptions,
+) {
+  const {
+    withInput: { inputArgOptions },
+  } = this.options;
+
+  const inputRef = this.inputType(inputName, {
+    ...inputOptions,
+    fields: inputFields,
+  });
+
+  this.interfaceField(ref, fieldName, (t) =>
+    t.field({
+      ...fieldOptions,
+      args: {
+        [argName]: t.arg({ ...(inputArgOptions as {}), type: inputRef, required: true }),
+      },
+    } as never),
+  );
+};

packages/plugin-with-input/src/types.ts
@@ -1,0 +1,132 @@
+import {
+  FieldNullability,
+  InputFieldMap,
+  InputFieldRef,
+  InputRef,
+  InputShapeFromFields,
+  SchemaTypes,
+  TypeParam,
+} from '@pothos/core';
+
+export interface WithInputBuilderOptions<Types extends SchemaTypes> {
+  inputArgOptions: Omit<
+    PothosSchemaTypes.ArgFieldOptions<Types, InputRef<{}>, true>,
+    'fields' | 'required' | 'type'
+  >;
+}
+
+export type WithInputInputOptions<
+  Types extends SchemaTypes,
+  Fields extends InputFieldMap,
+  InputName extends string,
+> = Omit<PothosSchemaTypes.InputObjectTypeOptions<Types, Fields>, 'fields'> & {
+  name?: string;
+  argName?: InputName;
+  inputFields: (t: PothosSchemaTypes.InputFieldBuilder<Types, 'InputObject'>) => Fields;
+};
+
+export type QueryFieldWithInputOptions<
+  Types extends SchemaTypes,
+  Fields extends InputFieldMap,
+  Type extends TypeParam<Types>,
+  Nullable extends FieldNullability<Type>,
+  InputName extends string,
+  ResolveReturnShape,
+> = Omit<
+  PothosSchemaTypes.QueryFieldOptions<
+    Types,
+    Type,
+    Nullable,
+    {
+      [K in InputName]: InputFieldRef<InputShapeFromFields<Fields>>;
+    },
+    ResolveReturnShape
+  >,
+  'args'
+>;
+
+export type MutationFieldWithInputOptions<
+  Types extends SchemaTypes,
+  Fields extends InputFieldMap,
+  Type extends TypeParam<Types>,
+  Nullable extends FieldNullability<Type>,
+  InputName extends string,
+  ResolveReturnShape,
+> = Omit<
+  PothosSchemaTypes.MutationFieldOptions<
+    Types,
+    Type,
+    Nullable,
+    {
+      [K in InputName]: InputFieldRef<InputShapeFromFields<Fields>>;
+    },
+    ResolveReturnShape
+  >,
+  'args'
+>;
+
+export type SubscriptionFieldWithInputOptions<
+  Types extends SchemaTypes,
+  Fields extends InputFieldMap,
+  Type extends TypeParam<Types>,
+  Nullable extends FieldNullability<Type>,
+  InputName extends string,
+  ResolveShape,
+  ResolveReturnShape,
+> = Omit<
+  PothosSchemaTypes.SubscriptionFieldOptions<
+    Types,
+    Type,
+    Nullable,
+    {
+      [K in InputName]: InputFieldRef<InputShapeFromFields<Fields>>;
+    },
+    ResolveShape,
+    ResolveReturnShape
+  >,
+  'args'
+>;
+
+export type ObjectFieldWithInputOptions<
+  Types extends SchemaTypes,
+  ParentShape,
+  Fields extends InputFieldMap,
+  Type extends TypeParam<Types>,
+  Nullable extends FieldNullability<Type>,
+  InputName extends string,
+  ResolveReturnShape,
+> = Omit<
+  PothosSchemaTypes.ObjectFieldOptions<
+    Types,
+    ParentShape,
+    Type,
+    Nullable,
+    {
+      [K in InputName]: InputFieldRef<InputShapeFromFields<Fields>>;
+    },
+    ResolveReturnShape
+  >,
+  'args'
+>;
+
+export type InterfaceFieldWithInputOptions<
+  Types extends SchemaTypes,
+  ParentShape,
+  Fields extends InputFieldMap,
+  Type extends TypeParam<Types>,
+  Nullable extends FieldNullability<Type>,
+  InputName extends string,
+  ResolveReturnShape,
+> = Omit<
+  PothosSchemaTypes.InterfaceFieldOptions<
+    Types,
+    ParentShape,
+    Type,
+    Nullable,
+    {
+      [K in InputName]: InputFieldRef<InputShapeFromFields<Fields>>;
+    },
+    ResolveReturnShape
+  >,
+  'args'
+>;

packages/plugin-with-input/tests/example/builder.ts
@@ -1,0 +1,11 @@
+import SchemaBuilder from '@pothos/core';
+import WithInputPlugin from '../../src';
+
+export default new SchemaBuilder({
+  plugins: [WithInputPlugin],
+  withInput: {
+    inputArgOptions: {
+      description: 'input arg',
+    },
+  },
+});