0b87f2b5cccd5629fc38d32fbdc793a7bcfb7fa2
Jonne Deprez <jonne.deprez@gmail.com>
2022-09-17 20:45:15 +0200

    Add support for inline composite rules to AuthZ plugin

--- packages/plugin-authz/src/types.ts
+++ packages/plugin-authz/src/types.ts
@@ -1,5 +1,19 @@
 import { SchemaTypes } from '@pothos/core';
 
-export interface AuthZOption<Types extends SchemaTypes> {
-  rules: Types['AuthZRule'][];
-}
+type RequireAtLeastOne<T> = {
+  [K in keyof T]-?: Required<Pick<T, K>> & Partial<Pick<T, Exclude<keyof T, K>>>;
+}[keyof T];
+
+type CompositeRules<Types extends SchemaTypes> = RequireAtLeastOne<{
+  and?: Types['AuthZRule'][];
+  or?: Types['AuthZRule'][];
+  not?: Types['AuthZRule'];
+}>[];
+
+export type AuthZOption<Types extends SchemaTypes> =
+  | {
+      rules: Types['AuthZRule'][];
+    }
+  | {
+      compositeRules: CompositeRules<Types>;
+    };
