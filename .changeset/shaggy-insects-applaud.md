b1cabe4472db60b204270d05cc23a4c9b793d299
Michael Hayes <michael@hayes.io>
2022-12-14 11:17:25 -0800

    fix nullable input in non-strict mode

---
+++ .changeset/shaggy-insects-applaud.md
@@ -0,0 +1,5 @@
+---
+'@pothos/core': patch
+---
+
+Fix types for nullable inputs in non-strict mode
--- packages/core/src/types/utils.ts
+++ packages/core/src/types/utils.ts
@@ -7,11 +7,10 @@ export type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) ex
   ? I
   : never;
 
-export type RequiredKeys<T extends object> = {
-  [K in keyof T]: T[K] extends NonNullable<T[K]> ? K : never;
-}[keyof T];
+export type RequiredKeys<T extends object> = Exclude<keyof T, OptionalKeys<T>>;
+
 export type OptionalKeys<T extends object> = {
-  [K in keyof T]: T[K] extends NonNullable<T[K]> ? never : K;
+  [K in keyof T]: T[K] | undefined extends T[K] ? K : T[K] | null extends T[K] ? K : never;
 }[keyof T];
 
 export type NonEmptyKeys<T extends object> = {
