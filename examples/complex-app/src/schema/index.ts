0264dff85477162ade86957a674654fdd8f8fd45
Michael Hayes <michael@hayes.io>
2023-08-05 10:30:45 -0700

    Add viewer to complex app

--- examples/complex-app/src/schema/index.ts
+++ examples/complex-app/src/schema/index.ts
@@ -2,6 +2,7 @@ import './game';
 import './player';
 import './point';
 import './team';
+import './user';
 import { builder } from '../builder';
 
 export const schema = builder.toSchema();
---
+++ examples/complex-app/src/schema/user.ts
@@ -0,0 +1,66 @@
+import { builder } from '../builder';
+import { db } from '../db';
+
+const User = builder.prismaInterface('User', {
+  include: {
+    roles: {
+      select: {
+        role: true,
+      },
+    },
+  },
+  fields: (t) => ({
+    id: t.exposeID('id'),
+    name: t.exposeString('name'),
+  }),
+  resolveType: (user) => {
+    if (user.roles.some(({ role }) => role === 'ADMIN')) {
+      return 'Admin';
+    }
+
+    return 'Member';
+  },
+});
+
+export const Member = builder.prismaObject('User', {
+  include: {
+    roles: {
+      select: {
+        role: true,
+      },
+    },
+  },
+  variant: 'Member',
+  interfaces: [User],
+});
+
+export const Admin = builder.prismaObject('User', {
+  include: {
+    roles: {
+      select: {
+        role: true,
+      },
+    },
+  },
+  variant: 'Admin',
+  interfaces: [User],
+});
+
+builder.queryField('viewer', (t) =>
+  t.prismaField({
+    type: User,
+    nullable: true,
+    resolve: (query, root, args, ctx) => {
+      if (!ctx.user) {
+        return null;
+      }
+
+      return db.user.findUnique({
+        ...query,
+        where: {
+          id: ctx.user.id,
+        },
+      });
+    },
+  }),
+);
