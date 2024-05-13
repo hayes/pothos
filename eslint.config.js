55c26ba625d43fd4783dcc7acbb07f15d4521188
Michael Hayes <michael@hayes.io>
2024-05-13 10:24:26 -0700

    disable import extensions lint rule in test examples

--- eslint.config.js
+++ eslint.config.js
@@ -179,6 +179,7 @@ module.exports = [
       '@typescript-eslint/no-unsafe-member-access': 'off',
       '@typescript-eslint/no-unsafe-call': 'off',
       '@typescript-eslint/no-misused-promises': 'off',
+      'import/extensions': 'off',
     },
   },
 ];
