51cb1636899acf6aecf037498fb155ab3617f2e2
Michael Hayes <michael@hayes.io>
2025-03-13 15:42:59 -0700

    wip test

--- packages/plugin-validation/src/types.ts
+++ packages/plugin-validation/src/types.ts
@@ -1,6 +1,6 @@
-import type { MaybePromise, SchemaTypes } from '@pothos/core';
+import type { SchemaTypes } from '@pothos/core';
 import type { GraphQLResolveInfo } from 'graphql';
-import type { ZodError, ZodSchema } from 'zod';
+import type { ZodError } from 'zod';
 
 export interface ValidationPluginOptions<Types extends SchemaTypes> {
   validationError?: ValidationErrorFn<Types>;
