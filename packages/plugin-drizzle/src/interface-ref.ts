f68acc7ec0dbdd1bdd36f9cdae9cfab10117fe0c
Iha Shin <me@xiniha.dev>
2025-04-25 18:19:11 +0900

    include DrizzleNodeRef to DrizzleRef

--- packages/plugin-drizzle/src/interface-ref.ts
+++ packages/plugin-drizzle/src/interface-ref.ts
@@ -1,11 +1,15 @@
 import { InterfaceRef, type SchemaTypes } from '@pothos/core';
+import type { DrizzleNodeRef } from './node-ref';
 import { type DrizzleObjectRef, drizzleTableKey } from './object-ref';
 
 export type DrizzleRef<
   Types extends SchemaTypes,
   Table extends keyof Types['DrizzleRelationsConfig'] = keyof Types['DrizzleRelationsConfig'],
   T = {},
-> = DrizzleInterfaceRef<Types, Table, T> | DrizzleObjectRef<Types, Table, T>;
+> =
+  | DrizzleInterfaceRef<Types, Table, T>
+  | DrizzleObjectRef<Types, Table, T>
+  | DrizzleNodeRef<Types, Table, T>;
 
 export class DrizzleInterfaceRef<
   Types extends SchemaTypes,
