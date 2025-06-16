0e526680ec9c06f0d13280b1d1f64173c92a7241
Rom <romka@monday.com>
2025-06-16 17:26:25 +0300

    formatting

--- packages/plugin-federation/src/index.ts
+++ packages/plugin-federation/src/index.ts
@@ -1,5 +1,5 @@
-import "./global-types";
-import "./schema-builder";
+import './global-types';
+import './schema-builder';
 import SchemaBuilder, {
   BasePlugin,
   type PothosEnumValueConfig,
@@ -8,26 +8,19 @@ import SchemaBuilder, {
   type PothosTypeConfig,
   type SchemaTypes,
   type TypeParam,
-} from "@pothos/core";
-import { defaultFieldResolver } from "graphql";
-import { providesMap } from "./external-ref";
-import {
-  addUsedDirectives,
-  entityMapping,
-  keyDirective,
-  mergeDirectives,
-} from "./util";
-export { hasResolvableKey } from "./schema-builder";
+} from '@pothos/core';
+import { defaultFieldResolver } from 'graphql';
+import { providesMap } from './external-ref';
+import { addUsedDirectives, entityMapping, keyDirective, mergeDirectives } from './util';
+export { hasResolvableKey } from './schema-builder';
 
-export * from "./types";
+export * from './types';
 
-const pluginName = "federation";
+const pluginName = 'federation';
 
 export default pluginName;
 
-export class PothosFederationPlugin<
-  Types extends SchemaTypes
-> extends BasePlugin<Types> {
+export class PothosFederationPlugin<Types extends SchemaTypes> extends BasePlugin<Types> {
   override onTypeConfig(typeConfig: PothosTypeConfig) {
     const entityConfig = entityMapping.get(this.builder)?.get(typeConfig.name);
 
@@ -43,15 +36,13 @@ export class PothosFederationPlugin<
 
     const typeDirectives = [
       ...(entityConfig ? keyDirective(entityConfig.key) : []),
-      ...(entityConfig?.interfaceObject
-        ? [{ name: "interfaceObject", args: {} }]
-        : []),
+      ...(entityConfig?.interfaceObject ? [{ name: 'interfaceObject', args: {} }] : []),
       ...getCommonDirectives(typeConfig),
     ];
 
     addUsedDirectives(
       this.builder,
-      typeDirectives.map((d) => d.name)
+      typeDirectives.map((d) => d.name),
     );
 
     return {
@@ -59,58 +50,45 @@ export class PothosFederationPlugin<
       extensions: {
         ...typeConfig.extensions,
         apollo,
-        directives: mergeDirectives(
-          typeConfig.extensions?.directives as [],
-          typeDirectives
-        ),
+        directives: mergeDirectives(typeConfig.extensions?.directives as [], typeDirectives),
       },
     };
   }
 
   override onOutputFieldConfig(
-    fieldConfig: PothosOutputFieldConfig<Types>
+    fieldConfig: PothosOutputFieldConfig<Types>,
   ): PothosOutputFieldConfig<Types> | null {
-    const options =
-      fieldConfig.pothosOptions as PothosSchemaTypes.FieldOptionsByKind<
-        Types,
-        unknown,
-        TypeParam<Types>,
-        false,
-        {},
-        {},
-        {}
-      >["ExtendedEntity"];
+    const options = fieldConfig.pothosOptions as PothosSchemaTypes.FieldOptionsByKind<
+      Types,
+      unknown,
+      TypeParam<Types>,
+      false,
+      {},
+      {},
+      {}
+    >['ExtendedEntity'];
 
     const ref = Array.isArray(options.type) ? options.type[0] : options.type;
     const resolve = (
-      fieldConfig.kind === "ExternalEntity"
-        ? (defaultFieldResolver as never)
-        : fieldConfig.resolve
+      fieldConfig.kind === 'ExternalEntity' ? (defaultFieldResolver as never) : fieldConfig.resolve
     )!;
 
     const fieldDirectives = [
-      options.requires
-        ? { name: "requires", args: { fields: options.requires.selection } }
-        : null,
-      fieldConfig.kind === "ExternalEntity" ? { name: "external" } : null,
-      providesMap.has(ref)
-        ? { name: "provides", args: { fields: providesMap.get(ref) } }
-        : null,
+      options.requires ? { name: 'requires', args: { fields: options.requires.selection } } : null,
+      fieldConfig.kind === 'ExternalEntity' ? { name: 'external' } : null,
+      providesMap.has(ref) ? { name: 'provides', args: { fields: providesMap.get(ref) } } : null,
       fieldConfig.pothosOptions.override
-        ? { name: "override", args: fieldConfig.pothosOptions.override }
+        ? { name: 'override', args: fieldConfig.pothosOptions.override }
         : null,
       ...getCommonDirectives(fieldConfig),
     ].filter(Boolean) as { name: string }[];
 
     addUsedDirectives(
       this.builder,
-      fieldDirectives.map((d) => d.name)
+      fieldDirectives.map((d) => d.name),
     );
 
-    const directives = mergeDirectives(
-      fieldConfig.extensions?.directives as [],
-      fieldDirectives
-    );
+    const directives = mergeDirectives(fieldConfig.extensions?.directives as [], fieldDirectives);
 
     return {
       ...fieldConfig,
@@ -149,44 +127,42 @@ function getCommonDirectives<
         requireOneSlicingArgument?: boolean;
       };
     };
-  }
+  },
 >(config: T) {
   const tags =
-    typeof config.pothosOptions.tag === "string"
+    typeof config.pothosOptions.tag === 'string'
       ? [config.pothosOptions.tag]
-      : config.pothosOptions.tag ?? [];
+      : (config.pothosOptions.tag ?? []);
   const tagDirectives = tags.map((tag) => ({
-    name: "tag",
+    name: 'tag',
     args: { name: tag },
   }));
   const requiresScopes = config.pothosOptions.requiresScopes
     ? {
-        name: "requiresScopes",
+        name: 'requiresScopes',
         args: { scopes: config.pothosOptions.requiresScopes },
       }
     : null;
   const policy = config.pothosOptions.policy
-    ? { name: "policy", args: { policies: config.pothosOptions.policy } }
+    ? { name: 'policy', args: { policies: config.pothosOptions.policy } }
     : null;
   const cost =
     config.pothosOptions.cost !== undefined
-      ? { name: "cost", args: { weight: config.pothosOptions.cost } }
+      ? { name: 'cost', args: { weight: config.pothosOptions.cost } }
       : null;
   const listSize = config.pothosOptions.listSize
     ? {
-        name: "listSize",
+        name: 'listSize',
         args: Object.fromEntries(
-          Object.entries(config.pothosOptions.listSize).filter(
-            ([, v]) => v !== undefined
-          )
+          Object.entries(config.pothosOptions.listSize).filter(([, v]) => v !== undefined),
         ),
       }
     : null;
 
   return [
-    config.pothosOptions.inaccessible ? { name: "inaccessible" } : null,
-    config.pothosOptions.shareable ? { name: "shareable" } : null,
-    config.pothosOptions.authenticated ? { name: "authenticated" } : null,
+    config.pothosOptions.inaccessible ? { name: 'inaccessible' } : null,
+    config.pothosOptions.shareable ? { name: 'shareable' } : null,
+    config.pothosOptions.authenticated ? { name: 'authenticated' } : null,
     requiresScopes,
     policy,
     cost,
@@ -203,18 +179,15 @@ function addCommonDirectives<
       tag?: string[] | string;
       inaccessible?: boolean;
     };
-  }
+  },
 >(builder: PothosSchemaTypes.SchemaBuilder<Types>, config: T): T {
   const commonDirectives = getCommonDirectives(config);
   addUsedDirectives(
     builder,
-    commonDirectives.map((d) => d.name)
+    commonDirectives.map((d) => d.name),
   );
 
-  const directives = mergeDirectives(
-    config.extensions?.directives as [],
-    commonDirectives
-  );
+  const directives = mergeDirectives(config.extensions?.directives as [], commonDirectives);
 
   return {
     ...config,
